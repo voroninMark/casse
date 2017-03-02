569b910a0e4aee88675035cea2be1d84d4803969
markVoronin <voronin.mark123@gmail.com>
2017-03-02 14:00:32 +0100

    premier commit

---
+++ changeCasse.cpp
@@ -0,0 +1,20 @@
+// changeCasse.cpp
+// g++ -std=c++11 -Wall -Wextra -o changeCasse.out changeCasse.cpp
+#include <cctype>
+#include <iostream>
+int main(int argc, char* argv[])
+{
+    std::string texte = argv[1];
+    // change les minuscules par des majuscules et réciproquement
+    std::cout<<"modification no=1";
+    for (char & c : texte)
+    {
+        if (std::islower(c))
+            std::cout << char(std::toupper(c));
+        else if (std::isupper(c))
+            std::cout << char(std::tolower(c));
+        else 
+            std::cout << c;
+    }
+    return 0;
+}
